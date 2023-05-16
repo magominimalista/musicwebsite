e1324c64dbb0ae843f0e1a234487e92bf854bf18
Mago Minimalista <contato@magominimalista.com.br>
2023-05-16 06:08:21 -0300

    Finalizando os sliders

--- public/js/app.js
+++ public/js/app.js
@@ -190,6 +190,30 @@ next.addEventListener('click', () => {
     Array.from(document.getElementsByClassName('songItem'))[`${index-1}`].style.background = "rgb(105, 105, 170, .1)";
 })
 
+let left_scroll = document.getElementById('left_scroll');
+let right_scroll = document.getElementById('right_scroll');
+let pop_song = document.getElementsByClassName('pop_song')[0];
+
+left_scroll.addEventListener('click', () => {
+    pop_song.scrollLeft -= 330;
+})
+
+right_scroll.addEventListener('click', () => {
+    pop_song.scrollLeft += 330;
+})
+
+let left_scrolls = document.getElementById('left_scrolls');
+let right_scrolls = document.getElementById('right_scrolls');
+let item = document.getElementsByClassName('item')[0];
+
+left_scrolls.addEventListener('click', () => {
+    item.scrollLeft -= 330;
+})
+
+right_scrolls.addEventListener('click', () => {
+    item.scrollLeft += 330;
+})
+
 const songs = [
     {
         id:'1',
