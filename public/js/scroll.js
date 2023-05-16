ccf2bcd8f08cdb54ab02906376644b39b5abad8d
Mago Minimalista <contato@magominimalista.com.br>
2023-05-16 12:37:02 -0300

    Adicionando scroll

---
+++ public/js/scroll.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".menu_song").niceScroll({
+      cursorcolor: "rgb(105, 105, 170, .4)",
+      cursoropacitymax: 1,
+      touchbehavior: true,
+      cursorwidth: "7px",
+      cursorborder: "0",
+      cursorborderradius: "7px"
+    });
+
+    $(".song_side").niceScroll({
+        cursorcolor: "rgb(105, 105, 170, .4)",
+        cursoropacitymax: 1,
+        touchbehavior: true,
+        cursorwidth: "10px",
+        cursorborder: "0",
+        cursorborderradius: "10px"
+      });
+  });
